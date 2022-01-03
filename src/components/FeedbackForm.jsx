dbd677b3a94594de6fd631b7810fb997d8cb3a51
Catevika <dominique.bello@outlook.fr>
2022-01-03 18:36:10 +0100

    Add text validation to FeedbackForm

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,8 +4,23 @@ import Button from './shared/Button';
 
 function FeedbackForm() {
 	const [text, setText] = useState('');
+	const [btnDisabled, setBtnDisabled] = useState(true);
+	const [message, setMessage] = useState('');
 
-	const handleTextChange = (e) => setText(e.target.value);
+	const handleTextChange = (e) => {
+		if (text === '') {
+			setBtnDisabled(true);
+			setMessage(null);
+		} else if (text !== '' && text.trim().length <= 10) {
+			setBtnDisabled(true);
+			setMessage('Text must be at least 10 characters');
+		} else {
+			setBtnDisabled(false);
+			setMessage(null);
+		}
+
+		setText(e.target.value);
+	};
 
 	return (
 		<Card>
@@ -20,8 +35,11 @@ function FeedbackForm() {
 						placeholder='Write a Review'
 						value={text}
 					/>
-					<Button type='submit'>Send</Button>
+					<Button type='submit' isDisabled={btnDisabled}>
+						Send
+					</Button>
 				</div>
+				{message && <div className='message'>{message}</div>}
 			</form>
 		</Card>
 	);

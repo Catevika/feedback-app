777dc1f13c825f88a6045d77977148003a5af19c
Catevika <dominique.bello@outlook.fr>
2022-01-03 10:39:05 +0100

    Props & Styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,11 @@
-function Header({ text }) {
+function Header({ text, bgColor, textColor }) {
+	const headerstyles = {
+		backgroundColor: bgColor,
+		color: textColor
+	};
+
 	return (
-		<header>
+		<header style={headerstyles}>
 			<div className='container'>
 				<h2>{text}</h2>
 			</div>
@@ -9,7 +14,9 @@ function Header({ text }) {
 }
 
 Header.defaultProps = {
-	text: 'Feedback UI'
+	text: 'Feedback UI',
+	bgColor: 'rgba(0,0,0,0.4)',
+	textColor: '#ff6a95'
 };
 
 export default Header;

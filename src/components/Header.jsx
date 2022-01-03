cf852944d63f25dd7adb7fe86013f86074bb39d0
Catevika <dominique.bello@outlook.fr>
2022-01-03 11:59:00 +0100

    Add Card shared component + all PropTypes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 function Header({ text, bgColor, textColor }) {
 	const headerstyles = {
 		backgroundColor: bgColor,
@@ -17,6 +18,12 @@ Header.defaultProps = {
 	text: 'Feedback UI',
 	bgColor: 'rgba(0,0,0,0.4)',
 	textColor: '#ff6a95'
+};
+
+Header.propTypes = {
+	text: PropTypes.string,
+	bgColor: PropTypes.string,
+	textColor: PropTypes.string
 };
 
 export default Header;

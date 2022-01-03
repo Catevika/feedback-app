95ff39c1a0acd5101e144815df37d5afb34aee1c
Catevika <dominique.bello@outlook.fr>
2022-01-03 14:40:30 +0100

    Events & props drilling

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,12 +1,15 @@
 import PropTypes from 'prop-types';
-
 import Card from './shared/Card';
+import { FaTimes } from 'react-icons/fa';
 
-function FeedbackItem({ item }) {
+function FeedbackItem({ item, handleDelete }) {
 	const { rating, text } = item;
 	return (
 		<Card>
 			<div className='num-display'>{rating}</div>
+			<button onClick={() => handleDelete(item.id)} className='close'>
+				<FaTimes color='purple' />
+			</button>
 			<div className='text-display'>{text}</div>
 		</Card>
 	);

54ba8164d282e5fa04a9a9ecf7844b514e6721c7
Catevika <dominique.bello@outlook.fr>
2022-01-04 19:15:19 +0100

    Refactoring with Context

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,13 +1,17 @@
+import { useContext } from 'react';
 import PropTypes from 'prop-types';
 import Card from './shared/Card';
 import { FaTimes } from 'react-icons/fa';
+import FeedbackContext from '../context/FeedbackContext';
 
-function FeedbackItem({ item, handleDelete }) {
+function FeedbackItem({ item }) {
+	const { deleteFeedback } = useContext(FeedbackContext);
+
 	const { rating, text } = item;
 	return (
 		<Card>
 			<div className='num-display'>{rating}</div>
-			<button onClick={() => handleDelete(item.id)} className='close'>
+			<button onClick={() => deleteFeedback(item.id)} className='close'>
 				<FaTimes color='purple' />
 			</button>
 			<div className='text-display'>{text}</div>

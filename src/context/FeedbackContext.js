400faf41c94986401b27876135464e2b13ee9104
Catevika <dominique.bello@outlook.fr>
2022-01-05 14:23:00 +0100

    Add json-server + db.json to replace local data

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,31 +1,31 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-	const [feedback, setFeedback] = useState([
-		{
-			id: 1,
-			text: 'This item is feedback item 1',
-			rating: 10
-		},
-		{
-			id: 2,
-			text: 'This item is feedback item 2',
-			rating: 9
-		},
-		{
-			id: 3,
-			text: 'This item is feedback item 3',
-			rating: 7
-		}
-	]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [feedback, setFeedback] = useState([]);
 
 	const [feedbackEdit, setFeedbackEdit] = useState({
 		item: {},
 		edit: false
 	});
+
+	const fetchFeedback = async () => {
+		const response = await fetch(
+			'http://localhost:5000/feedback?_sort=id&_order=desc'
+		);
+
+		const data = await response.json();
+
+		setFeedback(data);
+		setIsLoading(false);
+	};
+
+	useEffect(() => {
+		fetchFeedback();
+	}, []);
 
 	const deleteFeedback = (id) => {
 		if (window.confirm('Are you sure you want to delete this item?')) {
@@ -62,6 +62,7 @@ export const FeedbackProvider = ({ children }) => {
 		<FeedbackContext.Provider
 			value={{
 				// states
+				isLoading,
 				feedback,
 				feedbackEdit, // * state of the edited item
 				// functions

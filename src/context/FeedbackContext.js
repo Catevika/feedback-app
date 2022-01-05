a61d29cb37e07ddad8e9ab78cbf579d6c495fe23
Catevika <dominique.bello@outlook.fr>
2022-01-05 15:32:16 +0100

    Update edit & updateFeedback for Json-server use

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -23,8 +23,11 @@ export const FeedbackProvider = ({ children }) => {
 		fetchFeedback();
 	}, []);
 
-	const deleteFeedback = (id) => {
+	const deleteFeedback = async (id) => {
 		if (window.confirm('Are you sure you want to delete this item?')) {
+			await fetch(`feedback/${id}`, {
+				method: 'DELETE'
+			});
 			setFeedback(feedback.filter((item) => item.id !== id));
 		}
 	};
@@ -50,11 +53,19 @@ export const FeedbackProvider = ({ children }) => {
 		});
 	};
 
-	const updateFeedback = (id, updatedItem) => {
+	const updateFeedback = async (id, updatedItem) => {
+		const response = await fetch(`/feedback/${id}`, {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(updatedItem)
+		});
+
+		const data = await response.json();
+
 		setFeedback(
-			feedback.map((item) =>
-				item.id === id ? { ...item, ...updatedItem } : item
-			)
+			feedback.map((item) => (item.id === id ? { ...item, ...data } : item))
 		);
 
 		setFeedbackEdit({
